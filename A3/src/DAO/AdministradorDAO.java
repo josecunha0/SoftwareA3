ce8c39396952ffa54b8dd886a017ebeb903e33a5
José Fernando <123557505+josefernandocunha@users.noreply.github.com>
2023-06-03 18:34:48 -0300

    Update AdministradorDAO.java

--- A3/src/DAO/AdministradorDAO.java
+++ A3/src/DAO/AdministradorDAO.java
@@ -16,16 +16,13 @@ public class AdministradorDAO {
     }
 
     public Connection getConexao() {
-
         Connection connection = null;
-
         try {
-
             // Carregamento do JDBC Driver
             String driver = "com.mysql.cj.jdbc.Driver";
             Class.forName(driver);
 
-            // Configurar a conex�o
+            // Configurar a conexão
             String server = "localhost"; //caminho do MySQL
             String database = "Dev";
             String url = "jdbc:mysql://" + server + ":3306/" + database + "?useTimezone=true&serverTimezone=UTC";
@@ -38,18 +35,87 @@ public class AdministradorDAO {
             if (connection != null) {
                 System.out.println("Status: Conectado!");
             } else {
-                System.out.println("Status: N�O CONECTADO!");
+                System.out.println("Status: NÃO CONECTADO!");
             }
 
             return connection;
 
-        } catch (ClassNotFoundException e) {  //Driver n�o encontrado
+        } catch (ClassNotFoundException e) {  //Driver não encontrado
             System.out.println("O driver nao foi encontrado. " + e.getMessage() );
             return null;
 
         } catch (SQLException e) {
             System.out.println("Nao foi possivel conectar...");
             return null;
+        }
+    }
+    
+    public boolean verificarLogin(String id, String senha) {
+        Connection conexao = getConexao();
+        String sql = "SELECT * FROM administrador WHERE id = ? AND senha = ?";
+        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+            stmt.setString(1, id);
+            stmt.setString(2, senha);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    
+    public void criarAdministrador(String id, String senha) {
+        Connection conexao = getConexao();
+        String sql = "INSERT INTO administrador (id, senha) VALUES (?, ?)";
+        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+            stmt.setString(1, id);
+            stmt.setString(2, senha);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Administrador lerAdministrador(String id) {
+        Connection conexao = getConexao();
+        String sql = "SELECT * FROM administrador WHERE id = ?";
+        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+            stmt.setString(1, id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    String senha = rs.getString("senha");
+                    return new Administrador(0, id, senha);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public void atualizarAdministrador(String id, String novaSenha) {
+        Connection conexao = getConexao();
+        String sql = "UPDATE administrador SET senha = ? WHERE id = ?";
+        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+            stmt.setString(1, novaSenha);
+            stmt.setString(2, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void excluirAdministrador(String id) {
+        Connection conexao = getConexao();
+        String sql = "DELETE FROM administrador WHERE id = ?";
+        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+            stmt.setString(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
